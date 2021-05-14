852ef654bbfc3947c2170d78e5f67df39c6f38ca
Shrishti34 <saxena.shrishti03@gmail.com>
2021-05-14 09:44:00 +0530

    new

---
+++ Score/src/score/score.java
@@ -0,0 +1,34 @@
+package score;
+
+import java.util.Scanner;
+
+public class score {
+
+	public static void main(String[] args) {
+		
+		int A,B,C,sum=0;
+		double avg = 0;
+		
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Student",i);
+		{
+		
+		System.out.println("Enter the A marks");
+		A = sc.nextInt();
+		
+		System.out.println("Enter the B marks");
+		B = sc.nextInt();
+		
+		System.out.println("Enter the C marks");
+		C = sc.nextInt();
+		
+		sum = A + B+ C;
+		avg = (A+B+C)/3;
+		}
+
+	}
+
+}
